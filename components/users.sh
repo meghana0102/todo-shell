f4eea5720aa9f94500ce03b7d1d0311ed46afe19
meghana0102 <82608974+meghana0102@users.noreply.github.com>
2021-07-27 23:37:37 +0530

    Update users.sh

--- components/users.sh
+++ components/users.sh
@@ -16,10 +16,8 @@ Stat $?
 DOWNLOAD_COMPONENT
 
 Head "Extract Downloaded Archive"
-cd /home/ubuntu && rm -rf users && unzip -o /tmp/users.zip  && mv users-main users  && cd users && mvn clean package && mv target/users-api-0.0.1.jar users.jar
+cd /home/ubuntu && rm -rf users && unzip -o /tmp/users.zip &>>$LOG && mv users-main users  && cd users && mvn clean package &>>$LOG && mv target/users-api-0.0.1.jar users.jar
 Stat $?
-
-
 
 Head "Setup SystemD Service"
 mv /home/ubuntu/users/systemd.service /etc/systemd/system/users.service && systemctl daemon-reload && systemctl start users && systemctl enable users &>>$LOG
